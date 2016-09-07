fb997a1cb1145ad160b7d4378cf65832163d6f93
Rod c <raadce@gmail.com>
2016-09-07 00:53:02 +0000

    adding bunsch of stuff

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,50 @@
 class WorkoutsController < ApplicationController
+    
+    
+    def index
+        @workouts = Workout.all
+    end
+    
+    def show
+    end
+    
+    
+    def new
+        @workout = Workout.new
+    end
+    
+    
+    def edit
+        
+    end
+    
+    
+    def create
+        @workout = Workout.new(workout_params)
+        if @workout.save
+            redirect_to workout_path
+        else
+            render 'new'
+        end
+    end
+    
+    
+    def updated
+        
+        
+    end
+    
+    
+    def destroy
+        
+    end
+    
+    
+    
+    
+    
+    private
+    def workout_params
+    params.require(:workout).permit(:date, :workout, :mood, :length)
+    end
 end
